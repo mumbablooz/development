7277a428d372340873b1bca58c3a1c8632920338
Blooz <89782115+mumbablooz@users.noreply.github.com>
2023-12-10 09:22:34 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import WebApps from './components/WebApps'
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-2">
+    <main 
+      className="flex min-h-screen flex-col items-center p-2"
+      style={{
+      width: '100vw'
+    }}>
 
 <h3>Meine Web Apps:</h3>
 <WebApps />
