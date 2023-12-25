0a844acb95d2573439fb33a26d7212a5bf47e2f4
Blooz <89782115+mumbablooz@users.noreply.github.com>
2023-12-25 20:32:22 +0100

    Aktualisieren von app

---
+++ app/bildLinks.js
@@ -0,0 +1,1 @@
+export const bildLinksArray = ['https://www.bing.com/images/create/ein-bild-mit-einem-computer2c-der-einen-code-schrei/1-6589d6032cf547a1a0ff65081da8ce7b?id=XZ8QDU8FB0daY1Gah4JXrg.tijPn7munc5J%2FraZVLEHJw&view=detailv2&idpp=genimg']
