18739b092c345c6f4b05a7a260af2943a1338400
Blooz <89782115+mumbablooz@users.noreply.github.com>
2023-12-25 20:44:25 +0100

    Aktualisieren von layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Image from 'next/image'
 import './globals.css'
-import {bilLinksArray} from './bildLinks'
+import {bidlLinksArray} from './bildLinks'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
