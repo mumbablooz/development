8f7c8c5e8e11f5a045f0cf174da8faa06a675bff
Blooz <89782115+mumbablooz@users.noreply.github.com>
2024-01-13 12:21:26 +0100

    Add files via upload

---
+++ app/components/dateParts/EmailButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EmailButton() {
+  return (
+    <a href={'mailto:info@blooz.de'} className="button"><p 
+        
+>E-Mail: info@blooz.de</p></a>
+  )
+}
