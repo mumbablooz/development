ec8410331675e8d6fbd769605ac0598b65995dac
Blooz <89782115+mumbablooz@users.noreply.github.com>
2024-01-13 13:25:24 +0100

    Add files via upload

---
+++ app/components/dateParts/HomePageButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function homePageButton() {
+  return (
+    <a href={'https://blooz.de'} 
+        className="button"
+><p        
+        >Homepage: blooz.de </p></a>
+  )
+}
