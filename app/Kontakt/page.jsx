f45dfd800a065913ca1a8f40d25d5e002dcf7590
Blooz <89782115+mumbablooz@users.noreply.github.com>
2024-01-13 12:22:30 +0100

    Update page.jsx

--- app/Kontakt/page.jsx
+++ app/Kontakt/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import KontaktDaten from '../components/KontaktDaten'
+import KontaktDaten from '../components/dateParts/KontaktDaten'
 import KontaktFormular from '../components/KontaktFormular/KontaktFormular'
 
 export default function Kontakt() {
@@ -8,7 +8,7 @@ export default function Kontakt() {
     <section>
         <h2>Kontakt</h2>
 <KontaktFormular />    
-
+<br />
 <KontaktDaten />
         <br />
      </section>
