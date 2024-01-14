d017cf01ef1d026f8fcd834e10a92ffbf9ecea04
Blooz <89782115+mumbablooz@users.noreply.github.com>
2024-01-14 14:26:25 +0100

    Delete app directory

--- app/components/GetIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Image from 'next/image'
-
-export default function GetIcon() {
-  return (<>
-  <Image src={'/bloozLogo'} width={800} height={800} alt='Icon' />
-  </>)
-
-}
