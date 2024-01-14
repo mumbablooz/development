33d28ce50b635619d015c226b0ac6152526bb698
Blooz <89782115+mumbablooz@users.noreply.github.com>
2024-01-14 10:31:38 +0100

    Add files via upload

--- app/components/spenden/PaypalSpendenButton.jsx
+++ app/components/spenden/PaypalSpendenButton.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
+
 export default function PayPalButton(){
+
     return (<Link 
     href={'https://paypal.me/markbortey?country.x=DE&locale.x=de_DE'}
-    className='greenButton'>
-      über Paypal spenden
+    className='greenButton'
+    style={{display: 'flex'}}>
+      <p style={{
+        marginRight: '0.5em'
+      }}>über Paypal spenden</p>
+      <img 
+      width="48" 
+      height="48" 
+      src="https://img.icons8.com/fluency/48/paypal.png" 
+      alt="paypal"/>  
       </Link>)
   }
