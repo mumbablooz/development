36c22410dba47c6b5875b124dda1a748e40d0c8f
Blooz <89782115+mumbablooz@users.noreply.github.com>
2024-01-14 10:30:38 +0100

    Add files via upload

--- app/components/KontaktButton.jsx
+++ app/components/KontaktButton.jsx
@@ -4,8 +4,15 @@ import Link from 'next/link'
 export default function KontaktButton() {
   return (
     <Link href={'/Kontakt'}
-    className='greenButton'>
-        Kontakt
+    className='button'>        
+        <p style={{
+        marginRight: '0.5em'
+      }}>Kontakt</p>
+        <img 
+        width="30" 
+        height="30" 
+        src="https://img.icons8.com/ios-glyphs/30/FFFFFF/mail-contact.png" 
+        alt="mail-contact"/>
     </Link>
   )
 }
