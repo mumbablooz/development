f3c5a15d5630921b7bab7519c7103b5d34b4a883
Blooz <89782115+mumbablooz@users.noreply.github.com>
2024-01-14 14:31:10 +0100

    Delete components directory

--- components/dateParts/EmailButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function EmailButton() {
-  return (
-    <a href={'mailto:info@blooz.de'} className="button"><p 
-        
->E-Mail: info@blooz.de</p></a>
-  )
-}
