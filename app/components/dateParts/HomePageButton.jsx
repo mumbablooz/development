7086f3a5c69814ebb0ce9ca6d1cf03efeb007a2f
Blooz <89782115+mumbablooz@users.noreply.github.com>
2024-02-20 09:47:54 +0100

    Delete app directory

--- app/components/dateParts/HomePageButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function HomePageButton() {
-  return (
-    <a href={'https://blooz.de'} 
-        className="button"
-><p>Homepage: blooz.de </p></a>
-  )
-}
